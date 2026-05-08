3d2517f86ff7306298fb345ca2f243b87e232a34
NojusGencas <nojus.gencas@stud.ktmc.lt>
2026-05-08 13:10:18 +0300

    Fix: Listen on 0.0.0.0 for Render compatibility

--- api/app.js
+++ api/app.js
@@ -38,6 +38,6 @@ app.use(function(err, req, res, next) {
 });
 
 const port = process.env.PORT || 3000;
-app.listen(port, "127.0.0.1", () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Programa veikia ant porto ${port}`);
 });
